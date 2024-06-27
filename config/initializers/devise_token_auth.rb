ee39f7baf8bda4fb3970e3a71726e0c58cbd31a1
Seamoon Pandey <hackedasocial@gmail.com>
2024-06-28 04:58:28 +0545

    dunno

--- config/initializers/devise_token_auth.rb
+++ config/initializers/devise_token_auth.rb
@@ -1,13 +1,7 @@
 # frozen_string_literal: true
 
 DeviseTokenAuth.setup do |config|
-  config.cookie_enabled = true
-  config.cookie_attributes = {
-    httponly: true,
-    encrypt: true,
-    same_site: :strict,
-    secure: true
-  }
+
   # By default the authorization headers will change after each request. The
   # client is responsible for keeping track of the changing tokens. Change
   # this to false to prevent the Authorization header from changing after
