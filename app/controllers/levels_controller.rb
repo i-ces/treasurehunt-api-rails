6b15a8e23b977c4fee54e1d76495b3db6868f4bc
Seamoon Pandey <hackedasocial@gmail.com>
2024-06-29 10:12:27 +0545

    remove authorization|

--- app/controllers/levels_controller.rb
+++ app/controllers/levels_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class LevelsController < ApplicationController
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
   before_action :set_level, only: %i[show destroy]
 
   # GET /levels
