498f06a57bfff152303658212f3cbc30ed969ebe
Seamoon Pandey <hackedasocial@gmail.com>
2024-06-29 08:55:55 +0545

    upload profile_image

---
+++ app/controllers/user/registrations_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class User::RegistrationsController < ApplicationController
+  def create
+    @user = User.create!(user_params)
+    render json:{
+      user: @user,
+      image: @user.profile_image_url
+    }
+  end
+  private
+    def user_params
+      params.permit(:email, :password, :name, :profile_image)
+    end
+end
