ab0a4d7c006b98294e992b9f26fde5ebbddbd0a2
Seamoon Pandey <hackedasocial@gmail.com>
2024-06-29 14:25:38 +0545

    another typo

--- config/initializers/active_storage_azure.rb
+++ config/initializers/active_storage_azure.rb
@@ -3,7 +3,7 @@ require "active_storage/service/azure_storage_service"
 Rails.application.config.active_storage.service_configurations = {
   microsoft: {
     service: "AzureStorage",
-    storage_account_name: ENV["AZURE_STORAGE_ACCOUNT_NAME"],
+    storage_account_name: "hackandhunt",
     storage_access_key: ENV["AZURE_STORAGE_ACCESS_KEY"],
     container: ENV["AZURE_STORAGE_CONTAINER"],
     public: true
