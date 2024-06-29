facbebeef01436e0b17c6b58a7c556b80584d99a
Seamoon Pandey <hackedasocial@gmail.com>
2024-06-29 15:23:13 +0545

    add service url

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,14 +42,14 @@ class User < ActiveRecord::Base
   has_one :user_level_progress, dependent: :destroy
   after_create :initialize_progress
 
-  has_one_attached :profile_image, dependent: :destroy
+  has_one_attached :profile_image
 
   validates :email, presence: true, uniqueness: true
   validates :name, presence: true
 
   def profile_image_url
     if profile_image.attached?
-      Rails.application.routes.url_helpers.url_for(profile_image)
+      profile_image.service_url
     end
   end
 
